f8df69b47957a0a6e8ee03f62d2a9aa90c387a22
Amey Dabhade <ameydabhade4@gmail.com>
2025-01-19 16:56:40 +0530

    Ready to host

--- EventManagement/src/components/EventPage.jsx
+++ EventManagement/src/components/EventPage.jsx
@@ -66,27 +66,30 @@ function EventPage() {
   });
 
   return (
-    <div className="container mx-auto p-6">
-      <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-md">
+    <div className="container mx-auto p-6 bg-black min-h-screen ">
+      <div className="max-w-4xl mx-auto bg-black bg-opacity-90 text-white border-[#7a56d6] border-2 p-6 rounded-lg shadow-md">
         <img
           src={event.image || "/placeholder.png"} // Fallback image if none exists
           alt={event.title}
           className="w-full h-64 object-cover rounded-md mb-6"
         />
-        <h2 className="text-3xl font-semibold mb-2">{event.title}</h2>
-        <p className="text-gray-500 mb-4">{formattedDate}</p> {/* Display formatted date */}
+        <h2 className="text-3xl font-semibold mb-2 text-white">{event.title}</h2>
+        <p className="text-white text-xl mb-4">{formattedDate}</p> {/* Display formatted date */}
         <p className="flex items-center mb-4">
           <i className="fas fa-map-marker-alt mr-2"></i>
           {event.location}
         </p>
-        <p className="text-lg">{event.description || "No description available"}</p>
+
+        {/* Add description text */}
+        <p className="text-white mb-6">
+           Desctiption example : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia, dui et lobortis posuere, odio felis viverra ligula, id cursus odio risus eget augue. Nunc facilisis justo sed ipsum porttitor, sit amet feugiat lorem euismod. Integer sed arcu eu ante gravida feugiat. Donec bibendum augue vel erat interdum, vel scelerisque ante blandit. Sed dapibus lorem eget nulla eleifend, non fermentum arcu auctor. Phasellus a libero in nisl fringilla suscipit sed ac libero. Fusce sit amet sapien velit. Suspendisse potenti. Curabitur dictum gravida tincidunt.
+        </p>
 
         {/* Show Delete button if the user is an admin */}
         {userRole === "admin" && (
           <button
             onClick={handleDelete}
-            className="mt-6 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-[#fa4d56] focus:ring-offset-2 transition duration-150 ease-in-out"
-          >
+            className="mt-6 w-full text-white py-2 px-6 rounded-lg border-2 border-transparent hover:text-[#7a56d6] border-white hover:border-[#7a56d6] transition-colors duration-300">
             Delete Event
           </button>
         )}
